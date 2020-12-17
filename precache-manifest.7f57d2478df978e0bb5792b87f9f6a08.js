4d8a90c1b6158a4c7610b47a0286fae427764a15
refresh2021 <irtijanafis@gmail.com>
2020-12-16 19:34:16 -0700

    Updates

---
+++ precache-manifest.7f57d2478df978e0bb5792b87f9f6a08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e1b1be2bfcb5f5cce483a80d7dcd9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7c112808b68fa0ff72c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6d54d602f3f4aab6ce6e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7c112808b68fa0ff72c",
+    "url": "/static/js/2.18e525e8.chunk.js"
+  },
+  {
+    "revision": "bb6cdbbd713c49682554a7acb160ff65",
+    "url": "/static/js/2.18e525e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d54d602f3f4aab6ce6e",
+    "url": "/static/js/main.7cd85798.chunk.js"
+  },
+  {
+    "revision": "8eba3c309f86cd64cebf",
+    "url": "/static/js/runtime-main.bbdacc90.js"
+  },
+  {
+    "revision": "35d07481aa47ace1c78d659866b130f9",
+    "url": "/static/media/paphos.35d07481.jpg"
+  }
+]);
